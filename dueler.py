9c631714c92eb2a40be3a46633142af01213338d
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-06 23:39:37 -0800

    init testing

--- dueler.py
+++ dueler.py
@@ -1,8 +1,22 @@
+# class Dog:
+#     def bark(self):
+#         print("Woof!")
+
+# my_dog = Dog()
+# my_dog.bark()
+
+# print(__name__)
+
+
 class Dog:
+    greeting = "Woof!"
+
+    def __init__(self, name):
+        self.name = name
+
     def bark(self):
-        print("Woof!")
+        print(self.greeting)
 
-my_dog = Dog()
-my_dog.bark()
 
-print(__name__)
+my_dog = Dog("Spot")
+print(my_dog.name)
