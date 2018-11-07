a3ce6cedd22be9a758a4a24f95cdc4c8d6334cc8
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-07 15:54:21 -0800

    creating dog objects

--- dueler.py
+++ dueler.py
@@ -18,5 +18,17 @@ class Dog:
         print(self.greeting)
 
 
+# creating dogs
 my_dog = Dog("Spot")
+dog1 = Dog("Ajax")
+dog2 = Dog("Cookie")
+
+# printing dog names
 print(my_dog.name)
+print(dog1.name)
+print(dog2.name)
+
+
+
+
+
